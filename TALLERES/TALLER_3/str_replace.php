4517e17e908dbad1277823f9336d15a5fe859ebb
John Miranda <mirandajohn634@gmail.com>
2025-09-01 18:46:50 -0500

    Añadido ejercicio de str_replace()

---
+++ TALLERES/TALLER_3/str_replace.php
@@ -0,0 +1,26 @@
+
+<?php
+// Ejemplo de uso de str_replace()
+$frase = "El gato negro saltó sobre el perro negro";
+$fraseModificada = str_replace("negro", "blanco", $frase);
+
+echo "Frase original: $frase</br>";
+echo "Frase modificada: $fraseModificada</br>";
+
+// Ejercicio: Crea una variable con una frase que contenga al menos tres veces la palabra "PHP"
+// y usa str_replace() para cambiar "PHP" por "JavaScript"
+$miFrase = "PHP es un lenguaje de programación. Me gusta aprender PHP y también enseñar PHP.";
+$miFraseModificada = str_replace("PHP", "JavaScript", $miFrase);
+
+echo "</br>Mi frase original: $miFrase</br>";
+echo "Mi frase modificada: $miFraseModificada</br>";
+
+// Bonus: Usa str_replace() para reemplazar múltiples palabras a la vez
+$texto = "Manzanas y naranjas son frutas. Me gustan las manzanas y las naranjas.";
+$buscar = ["Manzanas", "naranjas"];
+$reemplazar = ["Peras", "uvas"];
+$textoModificado = str_replace($buscar, $reemplazar, $texto);
+
+echo "</br>Texto original: $texto</br>";
+echo "Texto modificado: $textoModificado</br>";
+?>
