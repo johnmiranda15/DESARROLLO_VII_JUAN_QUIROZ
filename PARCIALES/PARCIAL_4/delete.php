1082670bccbfee26f25cbedcac0b12b2352fa4ab
John Miranda <mirandajohn634@gmail.com>
2025-12-01 19:50:31 -0500

    eliminar producto listo

---
+++ PARCIALES/PARCIAL_4/delete.php
@@ -0,0 +1,17 @@
+<?php
+require_once "config_mysqli.php";
+
+if(isset($_GET['id'])){
+    $id = intval($_GET['id']);
+    $sql = "DELETE FROM productos WHERE id = ?";
+    if($stmt = mysqli_prepare($conn, $sql)){
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+    }
+}
+
+mysqli_close($conn);
+header("Location: index.php");
+exit;
+?>
